c5758e762bfb8383feacb36522fd96924daeb4cf
Yunna Yap Yoon En <1019yoonen@gmail.com>
2024-06-17 04:39:52 +0800

    join room, leave room, send message, receive message

--- flasklivechatsocket/__init__.py
+++ flasklivechatsocket/__init__.py
@@ -74,6 +74,51 @@ def create_app():
         if room is None or session.get('name') is None or room not in rooms:
             return redirect(url_for('home'))
         
-        return render_template('room.html')
+        context = {
+            'room': room,
+            'messages': rooms[room]['messages']
+        }
+        return render_template('room.html', **context)
+    
+    @socketio.on('connect')
+    def connect(auth):
+        room = session.get('room')
+        name = session.get('name')
+        if not room or not name:
+            return
+        if room not in rooms:
+            leave_room(room)
+            return 
+        
+        join_room(room)
+        # send new member joined to specific room
+        send({'name': name, 'message': 'has entered the room'}, to=room)
+        rooms[room]['members'] += 1
+
+    @socketio.on('message')
+    def message(data):
+        room = session.get('room')
+        if room not in rooms:
+            return
+        
+        content = {
+            'name': session.get('name'),
+            'message': data['data']
+        }
+        send(content, to=room)
+        rooms[room]['messages'].append(content)
+
+    @socketio.on('disconnect')
+    def disconnect():
+        room = session.get('room')
+        name = session.get('name')
+        leave_room(room)
+
+        if room in rooms:
+            rooms[room]['members'] -= 1
+            if rooms[room]['members'] <= 0:
+                del rooms[room]
+
+        send({'name': name, 'message': 'has left the room'}, to=room)
 
     return app
